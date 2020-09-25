0ef79e34e4c8f122db31b0c633bcb11e69009986
Alex Sky <alex.sky23@yandex.ru>
2020-09-25 13:04:03 +0300

    first commit

--- init.js
+++ init.js
@@ -1,7 +1,33 @@
+var gen;
+var dat
+var monthes = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
 
-window.onload = function()
-{
-    const initPerson = personGenerator.getPerson();
-    document.getElementById('firstNameOutput').innerText = initPerson.firstName;
-};
+//window.onload = function()
+//{
+//    const initPerson = personGenerator.getPerson();
+//    document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+//};
 
+function gender() {
+    gen = Math.ceil(Math.random()*2);
+    gen = (gen < 2)? personGenerator.GENDER_MALE : personGenerator.GENDER_FEMALE;
+}
+
+document.getElementById('btn_gen').addEventListener('click', function() {
+    gender();
+
+// new Date() - сама генерирует правильные даты, т.е. 30 февраля она не позволит написать:
+    dat = new Date(personGenerator.randomIntNumber(), personGenerator.randomMont(), personGenerator.randomDay());
+    var dat_full = dat.getDate() + " " + monthes[dat.getMonth()] + " " + dat.getFullYear();
+    console.log(dat);
+
+    document.getElementById('firstNameOutput').innerText = personGenerator.randomFirstName();
+    document.getElementById('twoNameOutput').innerText = personGenerator.randomTwoName();
+    document.getElementById('surnameOutput').innerText = personGenerator.randomSurname();
+    document.getElementById('birthYearOutput').innerText = dat_full + " г.р.";
+    document.getElementById('genderOutput').innerText = gen;
+});
+
+document.getElementById('btn_off').addEventListener('click', function() {
+    history.go(0);
+});
--- personGenerator.js
+++ personGenerator.js
@@ -27,39 +27,77 @@ const personGenerator = {
             "id_2": "Максим",
             "id_3": "Иван",
             "id_4": "Артем",
-            "id_5": "Дмитрий",
-            "id_6": "Никита",
+            "id_5": "Даниил",
+            "id_6": "Егор",
             "id_7": "Михаил",
-            "id_8": "Даниил",
-            "id_9": "Егор",
+            "id_8": "Дмитрий",
+            "id_9": "Никита",
             "id_10": "Андрей"
+        }
+    }`,
+
+    firstNameFamaleJson: `{
+        "count": 10,
+        "list": {
+            "id_1": "Анастасия",
+            "id_2": "Анна",
+            "id_3": "Екатерина",
+            "id_4": "Тамара",
+            "id_5": "Олеся",
+            "id_6": "Ксения",
+            "id_7": "Елизавета",
+            "id_8": "Кристина",
+            "id_9": "Виктория",
+            "id_10": "Людмила"
         }
     }`,
 
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
 
-    randomIntNumber: (max = 1, min = 0) => Math.floor(Math.random() * (max - min + 1) + min),
-
+    randomIntNumber: (max = 2010, min = 1955) => Math.floor(Math.random() * (max - min + 1) + min),
+    randomMont: (max = 12, min = 1) => Math.floor(Math.random() * (max - min + 1) + min),
+    randomDay: (max = 31, min = 1) => Math.floor(Math.random() * (max - min + 1) + min),
+    
     randomValue: function (json) {
         const obj = JSON.parse(json);
         const prop = `id_${this.randomIntNumber(obj.count, 1)}`;  // this = personGenerator
         return obj.list[prop];
     },
 
-    randomFirstName: function() {
-
-        return this.randomValue(this.firstNameMaleJson);
-
+    randomValue_otch: function (json) {
+        const obj = JSON.parse(json);
+        const prop = `id_${this.randomIntNumber(6, 1)}`;  // this = personGenerator
+        return obj.list[prop];
     },
 
+    randomFirstName: function() {
+        
+            if(gen === "Мужчина"){
+            return this.randomValue(this.firstNameMaleJson);
+        }
+        else {
+            return this.randomValue(this.firstNameFamaleJson);
+        }
+    },
 
      randomSurname: function() {
-
+        if(gen === "Мужчина"){
         return this.randomValue(this.surnameJson);
-
+    }
+        else {
+            return this.randomValue(this.surnameJson) + "a";
+        }
     },
 
+    randomTwoName: function() {
+        if(gen === "Мужчина"){
+        return this.randomValue_otch(this.firstNameMaleJson) + "ович";
+    }
+        else {
+            return this.randomValue_otch(this.firstNameMaleJson) + "овна";
+        }
+    },
 
     getPerson: function () {
         this.person = {};
